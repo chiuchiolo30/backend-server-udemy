1949a863436bb5fcc167067bf4041b2d26dfac53
Edgardo Chiuchiolo <chiuchioloedgardo@gmail.com>
2020-01-28 00:40:31 -0300

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
+/*jshint esversion: 6 */ 
+
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
+// Importar Rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuarios');
+var loginRoutes = require('./routes/login');
 
 // Inicializar variables
 var app = express();
+
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 // Conexión a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDb', ( err, res ) => {
@@ -13,14 +26,11 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDb', ( err, res )
         console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
 });
 
-// Rutas
-app.get('/', (req, res, next ) => {
+// Rutas - Middleware
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realiza correctamente'
-    });
-});
 
 // Escuchar peticiones
 app.listen(3000, () => {
---
+++ middlewares/autenticacion.js
@@ -0,0 +1,33 @@
+/*jshint esversion: 6 */
+
+var jwt = require('jsonwebtoken');
+
+var SEED = require('../config/config').SEED;
+
+
+
+
+//============================================================================
+// Verificar token
+//============================================================================
+exports.verificaToken = function(req, res, next) {
+    var token = req.query.token;
+
+    jwt.verify( token, SEED, ( err, decode ) => {
+        if ( err ) {
+            return res.status(401).json({
+                ok: false,
+                mensaje: 'Token incorrecto',
+                errors: err
+            });
+        }
+        req.usuario = decode.usuario; // información del usuario que hizo la solicitud
+        next();
+    });
+};
+
+
+
+   
+
+
